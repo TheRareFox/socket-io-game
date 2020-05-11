e2cbba43d654b78e56114b763009eff977a34c9b
Zhang Yuxiang <47800388+UwUUwU@users.noreply.github.com>
2020-05-11 13:11:09 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -60,8 +60,7 @@ socket.on('create', (objects,linked) => {
 
 
 socket.on('del_user',(user,linked) =>{
-  console.log(user);
-  user = JSON.parse(user)[0];
+  user = JSON.parse(user);
   connected = linked;
   console.log(user);
   users = {};
@@ -77,8 +76,7 @@ socket.on('del_user',(user,linked) =>{
 });
 
 socket.on('add_user', user => {
-  console.log(user);
-  user = JSON.parse(user)[0];
+  user = JSON.parse(user);
   console.log(user);
   users = {};
   keys = [];
